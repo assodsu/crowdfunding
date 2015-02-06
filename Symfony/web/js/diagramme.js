b1f029ce79aa960046fcede4f1402784b9ded81d
Sebastienscout <schouteeten.sebastien@gmail.com>
2015-02-06 20:13:04 +0100

    Page projet + page accueil
    Page projet ajout contenu des sous-menus + modifs page accueil et menu

--- Symfony/web/js/diagramme.js
+++ Symfony/web/js/diagramme.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 	drawDiagramme();
 	var speed = 200;
-	$('#text-ressource-financiere').mouseover(function(){
+	$('#text-ressource-financiere').mouseenter(function(){
 		$('.pie').hide().html("<li class='visualize ressource-financiere' data-color='#d9534f' data-value='80'></li><li class='visualize' data-color='rgba(0,0,0,0)' data-value='20'></li>").fadeIn(speed);
 		$('.pie-content').hide().html("<div class='percentage'>80%</div><div class='name'>Financières</div>").fadeIn(speed);
 		drawDiagramme();
 	}).mouseleave(function(){
 		$('.pie').hide().html("<li class='visualize ressource-materiel' data-color='#5cb85c' data-value='20'></li><li class='visualize ressource-financiere' data-color='#d9534f' data-value='23'></li><li class='visualize' data-value='30' data-color='rgba(0,0,0,0)'></li><li class='visualize ressource-humain' data-color='#5bc0de' data-value='27'></li>").fadeIn(speed);
-		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Total</div>").fadeIn(speed);
+		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Ressources</div>").fadeIn(speed);
 		drawDiagramme();
 	});
-	$('#text-ressource-humaine').mouseover(function(){
+	$('#text-ressource-humaine').mouseenter(function(){
 		$('.pie').hide().html("<li class='visualize ressource-humain' data-color='#5bc0de' data-value='59'></li><li class='visualize' data-color='rgba(0,0,0,0)' data-value='41'></li>").fadeIn(speed);
 		$('.pie-content').hide().html("<div class='percentage'>59%</div><div class='name'>Humaines</div>").fadeIn(speed);
 		drawDiagramme();
 	}).mouseleave(function(){
 		$('.pie').hide().html("<li class='visualize ressource-materiel' data-color='#5cb85c' data-value='20'></li><li class='visualize ressource-financiere' data-color='#d9534f' data-value='23'></li><li class='visualize' data-value='30' data-color='rgba(0,0,0,0)'></li><li class='visualize ressource-humain' data-color='#5bc0de' data-value='27'></li>").fadeIn(speed);
-		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Total</div>").fadeIn(speed);
+		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Ressources</div>").fadeIn(speed);
 		drawDiagramme();
 	});
-	$('#text-ressource-materiel').mouseover(function(){
+	$('#text-ressource-materiel').mouseenter(function(){
 		$('.pie').hide().html("<li class='visualize ressource-materiel' data-color='#5cb85c' data-value='32'></li><li class='visualize' data-color='rgba(0,0,0,0)' data-value='68'></li>").fadeIn(speed);
 		$('.pie-content').hide().html("<div class='percentage'>32%</div><div class='name'>Matériels</div>").fadeIn(speed);
 		drawDiagramme();
 	}).mouseleave(function(){
 		$('.pie').hide().html("<li class='visualize ressource-materiel' data-color='#5cb85c' data-value='20'></li><li class='visualize ressource-financiere' data-color='#d9534f' data-value='23'></li><li class='visualize' data-value='30' data-color='rgba(0,0,0,0)'></li><li class='visualize ressource-humain' data-color='#5bc0de' data-value='27'></li>").fadeIn(speed);
-		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Total</div>").fadeIn(speed);
+		$('.pie-content').hide().html("<div class='percentage'>70%</div><div class='name'>Ressources</div>").fadeIn(speed);
 		drawDiagramme();
 	});
 	
